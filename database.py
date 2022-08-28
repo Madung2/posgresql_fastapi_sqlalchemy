53ad74844c2ca0b94f578ca770be9326691463e0
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-29 07:33:45 +0900

    feat crud done

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-from sqlite3 import SQLITE_ALTER_TABLE
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from .schemas import CreateJobRequest
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import Job
+
 
 app = FastAPI()
 
+@app.post('/')
+def create(details:CreateJobRequest,db:Session= Depends(get_db)):
+    to_create = Job(
+        title= details.title,
+        description= details.description
+    )
+    db.add(to_create)
+    db.commit()
+    return {
+        "success":True,
+        "created_id":to_create.id
+    }
+
+@app.get('/')
+def get_by_id(id:int, db:Session=Depends(get_db)):
+    return db.query(Job).filter(Job.id==id).first()
+
+
+@app.delete('/')
+def delete(id:int, db:Session=Depends(get_db)):
+    db.query(Job).filter(Job.id==id).delete()
+    db.commit()
+    return {'success':True}
 
 
 
